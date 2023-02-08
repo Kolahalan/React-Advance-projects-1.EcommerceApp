fac59d913fc3b9a0dad90cfc08605a4e59e0837c
Kolahalan R <kolahalan@psibertech.net>
2023-02-08 17:56:40 +0530

    Working on COUNT_CART_TOTALS in cart_context.js

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,7 +5,7 @@ import { FaCheck } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
-  // const { addToCart } = useCartContext();
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
@@ -55,15 +55,12 @@ const AddToCart = ({ product }) => {
           decrease={decrease}
           amount={amount}
         />
-        {/* 
+
         <Link
           to="/cart"
           className="btn"
           onClick={() => addToCart(id, mainColor, amount, product)}
         >
-          add to cart
-        </Link> */}
-        <Link to="/cart" className="btn">
           add to cart
         </Link>
       </div>
--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -5,22 +5,36 @@ import styled from "styled-components";
 import { useProductsContext } from "../context/products_context";
 import { useCartContext } from "../context/cart_context";
 import { useUserContext } from "../context/user_context";
-
-const CartButtons = () => {
+const CartButton = () => {
+  const { closeSidebar } = useProductsContext();
+  const { total_items, clearCart } = useCartContext();
+  //const { loginWithRedirect, myUser, logout } = useUserContext();
   return (
     <Wrapper className="cart-btn-wrapper">
-      {/* This className "cart-btn-wrapper" is used in NavBar's NavContainer styled component. Hence it is specified 
-          here to associate this Element to that NavContainer className  */}
-      <Link to="/cart" className="cart-btn">
+      <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
-        Login <FaUserPlus />
-      </button>
+      {/* {myUser ? (
+        <button
+          type="button"
+          className="auth-btn"
+          onClick={() => {
+            clearCart();
+            localStorage.removeItem("user");
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type="button" className="auth-btn" onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )} */}
     </Wrapper>
   );
 };
@@ -78,4 +92,4 @@ const Wrapper = styled.div`
     }
   }
 `;
-export default CartButtons;
+export default CartButton;
